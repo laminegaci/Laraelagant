358c17a887a5ae0ffadf2fb5d23c6c8e5caaa853
laminegaci <mohamed61lamine@gmail.com>
2021-07-01 16:18:06 +0100

    redesign dashboard

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -42,6 +42,11 @@
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
 
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+
 </head>
 
 <body class="skin-default-dark fixed-layout">
